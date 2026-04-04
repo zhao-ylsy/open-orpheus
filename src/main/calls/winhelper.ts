b57019dceace0778a581e0a43d4aa132fa585999
LuoRain <luotianyi@luotianyi.me>
2026-04-05 00:18:04 +0800

    chore: move manage open orpheus menu item

--- src/main/calls/winhelper.ts
+++ src/main/calls/winhelper.ts
@@ -292,8 +292,8 @@ registerCallHandler<MenuRequest, void>(
     const parsedMenuData = parseMenuData(data);
     for (let i = 0; i < parsedMenuData.content.length; i++) {
       const item = parsedMenuData.content[i];
-      if (item.menu_id === "exitApp") {
-        parsedMenuData.content.splice(i, 0, {
+      if (item.image_path && item.image_path.indexOf("menu/setting.svg") !== -1) {
+        parsedMenuData.content.splice(i + 1, 0, {
           menu: true,
           separator: false,
           enable: true,
